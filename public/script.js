4741d9e7cf6837c29f43f7f3e94ab37387416535
TimothyNevin <156096817+TimothyNevin@users.noreply.github.com>
2024-04-09 20:02:55 +0200

    make commit dog

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ const myPeer = new Peer(undefined, {
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
-const peers = {}
+
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
@@ -15,19 +15,12 @@ navigator.mediaDevices.getUserMedia({
 
   myPeer.on('call', call => {
     call.answer(stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
-    })
+
   })
 
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
-})
-
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
 })
 
 myPeer.on('open', id => {
